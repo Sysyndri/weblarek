f1228447e5b15cb37fc8b4b42276f17d923edecb
Mikhail Burkov <sy5yndrik@yandex.ru>
2026-01-01 12:31:29 +0300

    add description in and add interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,19 @@ export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
+
+export interface ProductItem {
+    id: string;
+    title: string;
+    image: string;
+    category: string;
+    price: number | null;
+    description: string
+}
+
+export interface Customer {
+    payment: 'card' | 'cash' | '';
+    address: string;
+    email: string;
+    phone: string;
+}
